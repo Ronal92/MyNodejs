8f8b6b26e5dfad2183f7829e5e1a1c4526e428ba
JinWoo-Heaven <21000189@handong.edu>
2017-06-07 16:20:26 +0900

    Making file uploading using multer

--- Nodejs+express/app_file.js
+++ Nodejs+express/app_file.js
@@ -1,12 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer'); // 파일 업로드를 위한 모듈
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+var upload = multer({ storage: _storage }); // 사용자가 올린 파일을 저장할 디렉토리
 var fs = require('fs');
 var app = express();  // express()은 애플리케이션 객체를 리턴한다.
-
 app.set('views', './views_folder');
 app.set('view engine', 'jade');
-
 app.use(bodyParser.urlencoded({ extended: false })); // bodyparser가 req에 body값을 준다.
+
+/*
+	사용자가 /user/sample1.png를 누르면 해당 사진을 uploads에서 가져와서 보여준다.
+*/
+app.use('/user', express.static('uploads'))
+/*
+	파일을 업로드하는 곳
+*/
+app.get('/upload', function (req, res) {
+	res.render('uploadForm');
+});
+/*
+	전송된 파일을 /upload 디렉토리에 저장하는 곳
+*/
+app.post('/upload', upload.single('userfile'), function (req, res){
+	console.log(req.file);
+	res.send('Upload');
+});
+
 
 /*
 	파일 생성하는 form을 제공한다.
@@ -47,7 +74,6 @@ app.get(['/topic', '/topic/:id'], function(req, res){
 		}
 	});
 });
-
 
 /*
 	파일이 생성되는 곳
